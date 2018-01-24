60b23c93b5fb76645b6b507e3fc74d43a9339343
Sam Gould <sjg326@gmail.com>
2018-01-24 08:20:34 -0800

    initialized starter files for winter olympics scheduler app in react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
